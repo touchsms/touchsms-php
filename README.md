cbfe93d6e66d7f6726f5813a115592d26a9ca491
touchsms <support@touchsms.com.au>
2015-04-16 10:59:17 +1000

    Inital Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# touchSMS Gateway Class
+
+### Get started
+
+Before getting started you must have a valid [api key](https://platform.touchsms.com.au/apis/) for touchSMS.
+
+### Installation
+
+Use [Composer](https://getcomposer.org/) to keep up to date.
+
+```
+$ composer require touchsms/touchsms
+```
+
+### Initialize the class
+
+```php
+$touchsms = new touchSMS('YOUR_API_ID', 'YOUR_API_PASSWORD');
+```
+
+### Examples
+
+Provided in the [examples](https://github.com/touchsms/touchsms-PHP-API/tree/master/example) folder.
---
+++ example/send-message.php
@@ -0,0 +1,21 @@
+<?php
+    require '../src/touchSMS.php';
+
+    use TouchSMS\TouchSMS\touchSMS;
+
+    $touchSMS = new touchSMS('YOUR_API_ID', 'YOUR_API_PASSWORD');
+    
+    /*
+        Mobile number provided by ACMA
+        http://www.acma.gov.au/Citizen/Consumer-info/All-about-numbers/Special-numbers/fictitious-numbers-for-radio-film-and-television-i-acma
+    */
+
+
+    $obj = $touchSMS->sendMessage('Hello World', '61491570110'); 
+
+    if($obj->code == 200) {
+        echo 'Message Sent';
+    } else {
+        echo '['.$obj->code.'] ';
+        echo $touchSMS->getCleanResponse();
+    }
