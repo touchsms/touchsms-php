57af6e84f9c92e465401a64fea19efdd86e06ee6
atymic <atymicq@gmail.com>
2024-02-10 16:30:37 +0100

    fix: ignore cache file

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -2,15 +2,6 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of JoliCode's Slack PHP API project.
- *
- * (c) JoliCode <coucou@jolicode.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__)
     ->exclude('docs')
