d8c490b1ab3597ce814075f63c56afd2e949a462
atymic <atymicq@gmail.com>
2024-02-17 14:52:35 +0100

    fix: update docs

--- examples/send-sms.php
+++ examples/send-sms.php
@@ -8,14 +8,12 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $client = ClientFactory::create(getenv('ACCESS_TOKEN'), getenv('TOKEN_ID'));
 
-$res = $client->sendMessages(new SendMessageBody([
-    'messages' => [ // Up to 1000 messages supported
-        new OutboundMessage([
-            'to' => '61491578888', // Test number
-            'from' => 'SHARED_NUMBER',
-            'body' => 'Text',
-        ]),
-    ],
+$res = $client->sendMessages((new SendMessageBody())->setMessages([ // Up to 1000 messages supported
+    new OutboundMessage([
+        'to' => '61491578888', // Test number
+        'from' => 'SHARED_NUMBER',
+        'body' => 'Text',
+    ]),
 ]));
 
 var_dump($res->getData()->getMessages()[0]); // Successful messages, \TouchSms\ApiClient\Api\Model\OutboundMessageResponse
@@ -23,6 +21,6 @@ var_dump($res->getData()->getErrors()); // Messages with errors, \TouchSms\ApiCl
 
 
 // If you wanted to dry send (ie, just calculate cost/etc)
-$client->sendMessagesDry(new SendMessageBody([
-    'messages' => [], // messages
+$client->sendMessagesDry((new SendMessageBody())->setMessages([
+    // messages
 ]));
